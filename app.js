fcc292f43e1c3a2104d2955e7c7295d1d239f08d
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-20 00:42:14 -0700

    Able to display pug with css

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 // FRAMEWORK FOR BUILDING WEB APPS WITH NODE
 const express = require('express');
+// LOGGING
+const morgan = require('morgan');
+// SIMPLIFY DIRECTORY PATHS
+const path = require('path');
+// MAKE THE APP
 const app = express();
 
-app.get('/', (req, res) => res.end('hey what is good?'));
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+// LOGGING
+app.use(morgan('combined'));
+// SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'styles')));
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
 
 app.listen(3000, () => console.log('i am listening on port 3000'));
